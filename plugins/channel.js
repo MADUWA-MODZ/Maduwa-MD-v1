309e0d6f67d137b967e7995e175647b6a643288d
Madumal <106644082+MADUWA-MODZ@users.noreply.github.com>
2024-03-21 01:27:23 +0530

    Add files via upload

---
+++ plugins/channel.js
@@ -0,0 +1,16 @@
+let handler = async m => m.reply(`
+
+╭⭑⭑⭑★✪•••*BOT*•••✪★⭑⭑⭑
+│ 📂 *BOT Name:* _MADUWA-ᎷᎠ-Ꮩ1_
+│ 📝 *Description:* _I'm MADUWA-MD-V1. A  
+┃MultiDevice WhatsApp bot with rich features  ┃Created BY +94784205098._
+│ 👤 *Owner:* _乇乂匚乇ㄥ_
+│ 🌐 *Channel:* https://whatsapp.com/channel
+┃/
+╰━━━━━━━━━━━━━━━━╯
+`.trim())
+handler.help = ['channel']
+handler.tags = ['main']
+handler.command = ['star', 'channel' ] 
+
+export default handler
